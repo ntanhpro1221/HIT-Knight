7693761bb9423b221e05000e9dbe437c8ab2c4ca
Nguyen Duy Tuan Anh <ntanhpro1221@gmail.com>
2024-10-15 18:02:30 +0700

    Implement HealthHandler

--- Assets/Scripts/HealthHandler/HealthHandler.cs
+++ Assets/Scripts/HealthHandler/HealthHandler.cs
@@ -1,18 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Manage current health of actor.
 /// </summary>
-public class HealthHandler : CoreComponent, IHealthHandler {
-    public bool IsDead => throw new System.NotImplementedException();
+public class HealthHandler : CoreComponent, IHealthHandler
+{
+    [SerializeField] protected float maxHp;
 
-    public BindableProperty<float> CurHealth => throw new System.NotImplementedException();
+    private readonly BindableProperty<float> curHp = new BindableProperty<float>();
+    public bool IsDead => curHp.Value <= 0;
 
-    public void Heal(float amount) {
-        throw new System.NotImplementedException();
+
+    public void Init(float newMaxHp)
+    {
+        this.maxHp = newMaxHp;
+        curHp.Value = maxHp;
+    }
+    public BindableProperty<float> CurHealth => curHp;
+    public void Heal(float amount)
+    {
+        curHp.Value = Mathf.Min( curHp.Value + amount,maxHp);
     }
 
-    public void TakeDamage(float damage) {
-        throw new System.NotImplementedException();
+    public void TakeDamage(float damage)
+    {
+        curHp.Value = Mathf.Max(curHp.Value - damage, 0);
     }
 }
