8a8a207f3f8a36d14b7cc71a07822e3b8db1d897
tranduc0102 <tranhuynhduc.vn@gmail.com>
2024-10-26 23:42:52 +0700

    Implement IBodyHandler
    Fix IBodyHandler
    
    fix

--- Assets/Scripts/BodyHandler/IBodyHandler.cs
+++ Assets/Scripts/BodyHandler/IBodyHandler.cs
@@ -1,27 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 /// <summary>
 /// Handle body of object.
 /// </summary>
 /// <typeparam name="T">Enum that describes all type of event that will be called by animation</typeparam>
 [RequireComponent(typeof(Animator))]
-public abstract class IBodyHandler<T> : IAnimUsable, IAnimHandlable<T> where T : Enum {
-    public void RegisterAnimEvent(T type, Action callback) {
-        throw new NotImplementedException();
+public abstract class IBodyHandler<T> : CoreComponent, IAnimUsable, IAnimHandlable<T> where T : Enum
+{
+    private Animator animator;
+    private Dictionary<T, Action> AnimEvents = new Dictionary<T, Action>();
+    protected virtual void Awake()
+    {
+        animator = GetComponent<Animator>();
     }
-    public void RemoveAnimEvent(T type, Action callback) {
-        throw new NotImplementedException();
+
+    protected virtual void Reset()
+    {
+        if (animator != null) return;
+        animator = GetComponent<Animator>();
     }
-    public void PostAnimEvent(T type) {
-        throw new NotImplementedException();
+    public void RegisterAnimEvent(T type, Action callback)
+    {
+        if (AnimEvents.ContainsKey(type))
+        {
+            AnimEvents[type] += callback;
+        }
+        else
+        {
+            AnimEvents.Add(type, null);
+            AnimEvents[type] += callback;
+        }
     }
-    public void PlayAnim(AnimInfo anim) {
-        throw new NotImplementedException();
+    public void RemoveAnimEvent(T type, Action callback)
+    {
+        if (!AnimEvents.ContainsKey(type))
+        {
+            Debug.Log("Not Found Type: " + type.GetType().Name);
+            return;
+        }
+        AnimEvents[type] -= callback;
     }
-    public void PlayAnim(AnimInfo anim, float normalizedTime) {
-        throw new NotImplementedException();
+    public void PostAnimEvent(T type)
+    {
+        if (!AnimEvents.ContainsKey(type))
+        {
+            Debug.Log("Event has no Listener");
+            return;
+        }
+
+        Action callback = AnimEvents[type];
+        if (callback != null)
+        {
+            callback();
+        }
+        else
+        {
+            Debug.Log("PostEvent " + type.GetType().Name + "but no listener remain, Remove this key");
+            AnimEvents.Remove(type);
+        }
     }
-    public void SetAnimLength(AnimInfo anim, float length) {
-        throw new NotImplementedException();
+    public void PlayAnim(AnimInfo anim)
+    {
+        if (anim != null)
+        {
+            animator.Play(anim.name);
+            animator.SetFloat(anim.speedVar, 1f);
+        }
+    }
+    public void PlayAnim(AnimInfo anim, float normalizedTime)
+    {
+        if (anim != null)
+        {
+            animator.Play(anim.name, 0, normalizedTime);
+        }
+    }
+    public void SetAnimLength(AnimInfo anim, float length)
+    {
+        if (anim == null)
+        {
+            Debug.LogWarning("AnimInfo is null.");
+            return;
+        }
+
+        if (animator == null)
+        {
+            Debug.LogWarning("Animator not found on this GameObject.");
+            return;
+        }
+        AnimationClip[] allAnimationClip = animator.runtimeAnimatorController.animationClips;
+        var animClip = allAnimationClip.FirstOrDefault(clip => clip.name.Equals(anim.name));
+        if (animClip != null)
+        {
+            float currSpeed = animClip.length;
+            float speed = currSpeed / length;
+            Debug.Log(currSpeed);
+            animator.SetFloat(anim.speedVar, speed);
+        }
+        else
+        {
+            Debug.LogWarning($"Animation with name containing '{anim.name}' not found in Animator.");
+        }
     }
 }
