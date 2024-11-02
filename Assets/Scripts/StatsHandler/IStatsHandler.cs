8d5129b97ffb2dda1abda8c9c3672abecd04302f
DuckCodeGame <141433125+anhnguyencoder@users.noreply.github.com>
2024-11-02 11:42:00 +0700

    StatsHandler

--- Assets/Scripts/StatsHandler/IStatsHandler.cs
+++ Assets/Scripts/StatsHandler/IStatsHandler.cs
@@ -1,23 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-/// <summary>
-/// Manage stats of actor with all buff
-/// </summary>
 public abstract class IStatsHandler<T> : CoreComponent where T : Enum {
-    /// <summary>
-    /// Current stats with all buff
-    /// </summary>
+    // lưu các buff hiện tại
+    private List<Buff<T>> buffs = new List<Buff<T>>();
+
+    // lưu chỉ số gốc
+    private Dictionary<T, float> rawStats = new Dictionary<T, float>();
+
+    // cs cuối
     public PropertySet<T, BindableProperty<float>> CurStats { get; }
-    /// <summary>
-    /// Add buff to actor
-    /// </summary>
+
     public void AddBuff(Buff<T> buff) {
-        throw new NotImplementedException();
+        buffs.Add(buff);
+        UpdateStats();
     }
-    /// <summary>
-    /// Set raw stat of actor (without any buff)
-    /// </summary>
+
+    public void RemoveBuff(Buff<T> buff) {
+        
+        buffs.Remove(buff);
+        UpdateStats();
+    }
+
+    //set default
     public void SetRawStat(T type, float value) {
-        throw new NotImplementedException();
+        rawStats[type] = value;
+        UpdateStats();
+    }
+    // tính chỉ số cuối
+    private void UpdateStats() {
+        foreach (T statType in Enum.GetValues(typeof(T))) {
+            float rawStat = rawStats.ContainsKey(statType) ? rawStats[statType] : 0;
+            float addBuff = buffs.Where(b => b.statType.Equals(statType) && b.buffType == BuffType.Add).Sum(b => b.value);
+            float mulBuff = buffs.Where(b => b.statType.Equals(statType) && b.buffType == BuffType.Mul).Sum(b => b.value);
+
+            float finalStat = rawStat + addBuff + (mulBuff * rawStat);
+
+            CurStats[statType].Value = finalStat;
+        }
+
+        // loại bỏ các buff hết thời gian
+        buffs.RemoveAll(b => b.existTime <= 0);
+    }
+    private void Update() {
+        UpdateStats();
     }
 }
+
