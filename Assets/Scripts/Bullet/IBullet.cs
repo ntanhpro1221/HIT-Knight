07b5928b6e3417305e41422145c9e6c64335a967
tranduc0102 <tranhuynhduc.vn@gmail.com>
2024-11-16 22:35:03 +0700

    Finsish Feature IBullet And LittleBullet

--- Assets/Scripts/Bullet/IBullet.cs
+++ Assets/Scripts/Bullet/IBullet.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// Bullet object.
 /// </summary>
 [RequireComponent(typeof(Collider2D))]
-public abstract class IBullet : MonoBehaviour {
+public abstract class IBullet : BulletBodyHandler {
     protected Core m_Core;
+    protected IRangedWeapon _weapon;
     /// <summary>
     /// Manage body
     /// </summary>
@@ -13,7 +15,14 @@ public abstract class IBullet : MonoBehaviour {
     /// <summary>
     /// Launch this bullet
     /// </summary>
-    public virtual void Launch(Transform root, Vector2 dir, IRangedWeapon weapon) {
-        throw new System.NotImplementedException();
+    public virtual void Launch(Transform root, Vector2 dir, IRangedWeapon weapon)
+    {
+        if (!weapon)
+        {
+            Debug.LogWarning("Weapon is NULL");
+            return;
+        }
+        root.Translate(dir*Time.deltaTime*weapon.StatsHandler.CurStats[WeaponStatType.BulletSpeed].Value);
+        _weapon = weapon;
     }
 }
--- Assets/Scripts/Bullet/LittleBullet.cs
+++ Assets/Scripts/Bullet/LittleBullet.cs
@@ -1,3 +1,25 @@
-﻿public class LittleBullet : IBullet {
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
+public class LittleBullet : IBullet {
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Enemy"))
+        {
+            return;
+        }
+        HealthHandler healthEnemy;
+        healthEnemy = other.GetComponent<HealthHandler>();
+        if (!healthEnemy)
+        {
+            healthEnemy = other.GetComponentInChildren<HealthHandler>();
+        }
+
+        if (!healthEnemy)
+        {
+            Debug.LogWarning($"{other.name} does not have a HealthHandler");
+            return;
+        }
+        healthEnemy.CurHealth.Value -= _weapon.StatsHandler.CurStats[WeaponStatType.Atk].Value;
+    }
 }
