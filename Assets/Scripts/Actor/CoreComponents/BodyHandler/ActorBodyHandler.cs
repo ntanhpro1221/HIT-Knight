c11ef5c93183750a8659d83dc252b73d14b11179
NGDtuanh <ntanhpro1221@gmail.com>
2024-11-16 15:52:48 +0700

    Implement ActorBodyHandler + BulletBodyHandler + WeaponBodyHandler (Fix IBodyHandler + AnimInfo)
    Implement BulletBodyHandler
    
    lmao

--- Assets/Scripts/Actor/CoreComponents/BodyHandler/ActorBodyHandler.cs
+++ Assets/Scripts/Actor/CoreComponents/BodyHandler/ActorBodyHandler.cs
@@ -1,4 +1,10 @@
 ﻿/// <summary>
 /// Common body handler of actor
 /// </summary>
-public class ActorBodyHandler : IBodyHandler<ActorAnimEventType> { }
+public class ActorBodyHandler : IBodyHandler<ActorAnimEventType> {
+    public static readonly AnimInfo Idle = new("Idle");
+    public static readonly AnimInfo Move = new("Move");
+    public static readonly AnimInfo Dash = new("Dash");
+    public static readonly AnimInfo Dead = new("Dead"); 
+    public static readonly AnimInfo Attack = new("Attack");
+}
