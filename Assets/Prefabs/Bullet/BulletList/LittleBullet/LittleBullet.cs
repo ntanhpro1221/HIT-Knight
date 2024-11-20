4cc2f1f4539b9ba7451737521f448b7947d6b971
NGDtuanh <ntanhpro1221@gmail.com>
2024-11-20 20:24:50 +0700

    Implement IBullet + LittleBullet (Add animation holder for animator controller of Actor, Weapon, Bulelt)

---
+++ Assets/Prefabs/Bullet/BulletList/LittleBullet/LittleBullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LittleBullet : IBullet {
+    protected virtual void OnTriggerEnter2D(Collider2D other) {
+        if (!other.CompareTag("Enemy")) return;
+
+        IHealthHandler targetHealth = other.gameObject.GetComponent<IActor>().HealthHandler;
+        targetHealth.TakeDamage(m_Weapon.StatsHandler.CurStats[WeaponStatType.Atk].Value);
+
+        BodyHandler.PlayAnim(BulletBodyHandler.Explode);
+        BodyHandler.RegisterAnimEvent(BulletAnimEventType.DONE_EXPLODE, OnDoneExplode);
+    }
+    private void OnDoneExplode() {
+        Destroy(gameObject); 
+    }
+    private void OnDestroy() {
+        BodyHandler.RemoveAnimEvent(BulletAnimEventType.DONE_EXPLODE, OnDoneExplode);
+    }
+}
--- Assets/Scripts/Bullet/LittleBullet.cs
+++
@@ -1,3 +0,0 @@
-﻿public class LittleBullet : IBullet {
-
-}
